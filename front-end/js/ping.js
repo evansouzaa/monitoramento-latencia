cd2f0adf717e53d7235ff31148e132040c2143e5
evandro souza pereira <evandro.souza.pereira@gmail.com>
2022-05-06 10:39:06 -0300

    Commit upload arquivos sistema monitoramento sirenes.

---
+++ front-end/js/ping.js
@@ -0,0 +1,58 @@
+function statusSirene(sirene, ip) {
+
+    function getRandom(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    const time = setInterval(() => {
+        httpRequest('###URL API', { "host": ip })
+    }, 10000);
+
+    function stopTime() {
+        alert("Ops! Algo deu errado... contate o administrador!")
+        clearInterval(time)
+    }
+
+    function defStatus(sirene, ping) {
+        const status = document.querySelector(`#${sirene}`)
+        if (ping.alive === false) {
+            status.classList.add('sirene-offline')
+            status.classList.remove('sirene-online')
+        }
+        if (ping.alive === true) {
+            status.classList.add('sirene-online')
+            status.classList.remove('sirene-offline')
+        }
+    }
+
+    function httpRequest(url, obj) {
+        (async() => {
+            try {
+                const rawResponse = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(obj)
+                });
+                const ping = await rawResponse.json();
+                //console.log(ping)
+                defStatus(sirene, ping)
+
+            } catch (erro) {
+                console.log(erro)
+                stopTime()
+            }
+        })();
+    }
+
+}
+
+const sirenes = document.querySelectorAll('.sirene')
+
+sirenes.forEach((element, index) => {
+    setTimeout(function() {
+        statusSirene(element.id, element.getAttribute('ip'))
+    }, index * 100)
+});
